67a6ff45c602f0dbe51acaf66b2b70e6dffe7401
Musclepanx <Musclepanx@users.noreply.github.com>
2020-05-29 18:25:48 +0200

    update

--- test.py
+++ test.py
@@ -1,7 +1,1 @@
-echo "# diazero" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/Musclepanx/diazero.git
-git push -u origin master
-                
+print("esto es una prueba")
